7546c6a2660c90eadd27801e8beed2f890ad70ee
Hladiuk <vitaliy.gladiuk@gmail.com>
2021-01-25 15:58:03 +0200

    OMS2

---
+++ scripts/MySQL566.sql
@@ -0,0 +1,13 @@
+use oms2;
+
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='1';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='2';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='3';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='4';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='5';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='6';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='7';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='8';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='9';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='10';
+UPDATE `oms2`.`Users` SET `IsUserActive`=1 WHERE `ID`='11';
